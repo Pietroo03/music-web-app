a753b8293b432559bab6dd66ba146f2093a9c77f
Pietroo03 <pietro.ponte.03@gmail.com>
2025-04-17 17:36:46 +0200

    add protection to routes

--- React-frontend/src/App.jsx
+++ React-frontend/src/App.jsx
@@ -12,6 +12,8 @@ import EditAlbumPage from './pages/EditAlbumPage';
 import EditArtistPage from './pages/EditArtistPage';
 import GenresPage from './pages/GenresPage'
 import CreateGenrePage from './pages/CreateGenre';
+import LoginPage from './pages/LoginPage';
+import ProtectedRoute from './components/security/ProtecteddRoute';
 
 function App() {
 
@@ -22,18 +24,49 @@ function App() {
         <Routes>
           <Route element={<AppLayout />}>
 
-            <Route path='/' element={<HomePage />}></Route>
+            <Route path='/' element={<LoginPage />}></Route>
+            <Route path='/home' element={<HomePage />}></Route>
             <Route path='/albums' element={<AlbumsPage />}></Route>
             <Route path='/artists' element={<ArtistsPage />}></Route>
             <Route path='/albums/:id' element={<SingleAlbumPage />}></Route>
             <Route path='/artists/:id' element={<SingleArtistPage />}></Route>
 
-            {/* <Route path='/albums/create' element={<CreateAlbumPage />}></Route>
-            <Route path='/genres' element={<GenresPage />}></Route>
-            <Route path='/artists/create' element={<CreateArtistPage />}></Route>
-            <Route path='/genres/create' element={<CreateGenrePage />}></Route>
-            <Route path='/albums/edit/:id' element={<EditAlbumPage />}></Route>
-            <Route path='/artists/edit/:id' element={<EditArtistPage />}></Route> */}
+            <Route path='/albums/create' element={
+              <ProtectedRoute requiredRole="ADMIN">
+                <CreateAlbumPage />
+              </ProtectedRoute>
+            }
+            />
+            <Route path='/artists/create' element={
+              <ProtectedRoute requiredRole="ADMIN">
+                <CreateArtistPage />
+              </ProtectedRoute>
+            }
+            />
+            <Route path='/genres' element={
+              <ProtectedRoute requiredRole="ADMIN">
+                <GenresPage />
+              </ProtectedRoute>
+            }
+            />
+            <Route path='/genres/create' element={
+              <ProtectedRoute requiredRole="ADMIN">
+                <CreateGenrePage />
+              </ProtectedRoute>
+            }
+            />
+            <Route path='/albums/edit/:id' element={
+              <ProtectedRoute requiredRole="ADMIN">
+                <EditAlbumPage />
+              </ProtectedRoute>
+            }
+            />
+            <Route path='/artists/edit/:id' element={
+              <ProtectedRoute requiredRole="ADMIN">
+                <EditArtistPage />
+              </ProtectedRoute>
+            }
+            />
 
           </Route>
         </Routes>
