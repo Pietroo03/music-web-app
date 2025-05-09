cb752c1c5b20ab962ba52cdfb6a9131ef56644b4
Pietroo03 <pietro.ponte.03@gmail.com>
2025-05-09 10:09:19 +0200

    add new folder for artists components

--- React-frontend/src/pages/ArtistsPage.jsx
+++ React-frontend/src/pages/ArtistsPage.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react"
-import ArtistCard from "../components/albumsComponents/ArtistCard"
+import ArtistCard from "../components/artistsComponents/ArtistCard"
 
 export default function ArtistsPage() {
 
